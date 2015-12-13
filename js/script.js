6f5444fce164b3912b31bd880d48c9f395cbde4f
Carlota Lazaro <alu0100698862@ull.edu.es>
2015-12-13 21:53:33 +0000

    Añadiendo eventos de jquery

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
- $("button").click(function() {
+ $("#boton").click(function() {
      alert("Hola Mundo con jQuery!");
    });
+
+  $("#logo").mouseover(function() {
+     $(this).animate({height:'150px'});
+  });
+
+  $("#logo").mouseout(function() {
+     $(this).animate({height:'50px'});
+  });
+
+  $("#ocultar").click(function() {
+    $("#msgid1").hide("slow")
+  });
+
+  $("#mostrar").click(function() {
+    $("#msgid1").show("slow")
+  });
+
+  $("#ocultar1").click(function(e){
+    $("#logo1").fadeOut(2000);
+  });
+
+  $("#mostrar1").click(function(e){
+    $("#logo1").fadeIn(2000);
+  });
+
 });
